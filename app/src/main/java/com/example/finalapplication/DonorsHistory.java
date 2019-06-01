6432c2e4938eb12a14488937ec70756b28b38c2d
Neeraj Gangrade Porwal <neerajgp4@gmail.com>
2019-06-01 16:37:21 +1000

    Changed few things in doaner history fixed few things

--- app/src/main/java/com/example/finalapplication/DonorsHistory.java
+++ app/src/main/java/com/example/finalapplication/DonorsHistory.java
@@ -1,13 +1,31 @@
 package com.example.finalapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DonorsHistory extends AppCompatActivity {
+
+    private Button goBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donors_history);
+
+        goBack = findViewById(R.id.btnGobackMain1);
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenMainPage();
+            }
+        });
+    }
+
+    public void OpenMainPage(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
